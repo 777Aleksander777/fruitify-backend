1b2f6de4d740aa559e0a2df9f4cb0621b5b8e37f
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 22:40:19 +0100

    plugin.ts

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -35,12 +35,18 @@
 // });
 
 module.exports = {
-  provider: "supabase",
-  providerOptions: {
-    apiUrl: process.env.SUPABASE_API_URL,
-    apiKey: process.env.SUPABASE_API_KEY,
-    bucket: process.env.SUPABASE_BUCKET,
-    directory: process.env.SUPABASE_DIRECTORY,
-    options: {}
-  }
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase",
+      providerOptions: {
+        apiUrl: process.env.SUPABASE_API_URL,
+        apiKey: process.env.SUPABASE_API_KEY,
+        bucket: process.env.SUPABASE_BUCKET,
+        directory: process.env.SUPABASE_DIRECTORY,
+        options: {
+          debug: true,
+        }
+      }
+    },
+  },
 }
