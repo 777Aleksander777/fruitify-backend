6e39b6ea7f6e307e6bf80032bda9bebc5bfe21f8
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 20:47:52 +0100

    supabase config 6

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -15,21 +15,14 @@
 //     },
 //   });
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: '@strapi/provider-upload-aws-s3',
-        providerOptions: {
-          s3Options: {
-            accessKeyId: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXNrc3pmd3pzZnBmamFtd3FiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTMxMDkzMSwiZXhwIjoyMDQ2ODg2OTMxfQ.aJVaXdB6CqVqz5zZaatkfIiHbU8D9R3RGkFkarfVNhE", // Supabase Service Key
-            secretAccessKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXNrc3pmd3pzZnBmamFtd3FiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTMxMDkzMSwiZXhwIjoyMDQ2ODg2OTMxfQ.aJVaXdB6CqVqz5zZaatkfIiHbU8D9R3RGkFkarfVNhE", // Może być tym samym co SUPABASE_KEY
-            region: 'eu-central-1', // Supabase nie wymaga regionu, ale warto podać.
-            endpoint: "https://feyskszfwzsfpfjamwqb.supabase.co/storage/v1", // Twoje API Supabase
-            params: {
-              Bucket: "Fruitify", // Nazwa zasobnika
-            },
-          },
-        },
-      },
+  upload: {
+    provider: 'supabase',
+    providerOptions: {
+      apiUrl: env('SUPABASE_API_URL'),
+      apiKey: env('SUPABASE_API_KEY'),
+      bucket: env('SUPABASE_BUCKET'),
+      directory: env('SUPABASE_DIRECTORY', 'uploads'), // Opcjonalnie
+      options: {}, // Możesz dodać dodatkowe opcje, np. kontrolę uprawnień
     },
-  });
-  
+  },
+});
