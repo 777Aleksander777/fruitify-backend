c0e216f1d63c94efd7f3048de137c93bc076fc44
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 17:18:59 +0100

    removing not-found button

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default async function NotFound() {
     return (
       <section className="max-w-[1536px] w-full min-h-screen px-8 mb-16 mx-auto flex flex-wrap flex-col justify-center align-center items-center gap-8">
           <h1>Pracujemy nad tym aby powstało tu coś pięknego.</h1>
-          <Button color="primary" as={Link} href="/">Strona główna</Button>
+          {/* <Button color="primary" as={Link} href="/">Strona główna</Button> */}
       </section>
     );
   }
