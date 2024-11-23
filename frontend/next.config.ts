1886279a46238c39710ce03132b4b29fd30a1f6d
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 17:12:22 +0100

    dodanie tailwindcss do webpack config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,14 @@ const path = require("path");
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export',
+  // webpack: (config) => {
+  //   config.resolve.alias["@"] = path.join(__dirname, "src");
+  //   return config;
+  // },
   webpack: (config) => {
-    config.resolve.alias["@"] = path.join(__dirname, "src");
+    config.resolve.alias['tailwindcss/plugin.js'] = path.resolve(
+      'node_modules/tailwindcss/plugin.js'
+    );
     return config;
   },
   images: {
