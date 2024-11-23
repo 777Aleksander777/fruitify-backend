70615f6d74125bfb0a6b8b2cdadb271f705b44f3
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 18:27:20 +0100

    supabase config 2

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,14 +1,32 @@
+// module.exports = ({ env }) => ({
+//     upload: {
+//       config: {
+//         provider: '@strapi/provider-upload-aws-s3',
+//         providerOptions: {
+//           accessKeyId: env('SUPABASE_KEY'), // Supabase Service Key
+//           secretAccessKey: env('SUPABASE_SECRET'), // Możesz użyć tego samego klucza
+//           region: 'us-east-1', // Domyślna wartość (Supabase nie wymaga regionu)
+//           endpoint: env('SUPABASE_URL') + '/storage/v1', // Twoje API Supabase
+//           params: {
+//             Bucket: env('SUPABASE_BUCKET'), // Nazwa Twojego zasobnika
+//           },
+//         },
+//       },
+//     },
+//   });
 module.exports = ({ env }) => ({
     upload: {
       config: {
         provider: '@strapi/provider-upload-aws-s3',
         providerOptions: {
-          accessKeyId: env('SUPABASE_KEY'), // Supabase Service Key
-          secretAccessKey: env('SUPABASE_SECRET'), // Możesz użyć tego samego klucza
-          region: 'us-east-1', // Domyślna wartość (Supabase nie wymaga regionu)
-          endpoint: env('SUPABASE_URL') + '/storage/v1', // Twoje API Supabase
-          params: {
-            Bucket: env('SUPABASE_BUCKET'), // Nazwa Twojego zasobnika
+          s3Options: {
+            accessKeyId: env('SUPABASE_KEY'), // Supabase Service Key
+            secretAccessKey: env('SUPABASE_SECRET'), // Może być tym samym co SUPABASE_KEY
+            region: 'us-east-1', // Supabase nie wymaga regionu, ale warto podać.
+            endpoint: env('SUPABASE_URL') + '/storage/v1', // Twoje API Supabase
+            params: {
+              Bucket: env('SUPABASE_BUCKET'), // Nazwa zasobnika
+            },
           },
         },
       },
