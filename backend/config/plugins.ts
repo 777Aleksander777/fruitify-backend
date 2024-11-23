4f7c4768895f3ba2949b3649d0dd0b775cfa66c0
777Aleksander777 <aleksanderb787@gmail.com>
2024-11-23 21:07:00 +0100

    supabase config 7

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -16,7 +16,7 @@
 //   });
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'supabase',
+    provider: 'strapi-provider-upload-supabase',
     providerOptions: {
       apiUrl: env('SUPABASE_API_URL'),
       apiKey: env('SUPABASE_API_KEY'),
